65f6eebb0dadfb425a9e0da563d6f01b76d5ac03
Igor Bodrov <bodrovigor02@gmail.com>
2023-05-19 16:51:56 +0300

    feat: 🎸 Разметка, стилизация, адаптив

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ let nav_list = document.querySelector('.main-nav__nav-list');
 burger.addEventListener('click', () => {
   burger.classList.toggle('main-nav__burger--active');
   mobile_menu.classList.toggle('main-nav__nav-menu--mobile--active');
-  page_cover.classList.toggle('cover-default--active');
+  document.body.classList.toggle('body-cover');
 
   if (burger.classList.contains('main-nav__burger--active')) {
     lock(mobile_menu);
@@ -18,6 +18,16 @@ burger.addEventListener('click', () => {
     unlock(mobile_menu);
   }
 })
+
+//- Закрытие по клику на пустое место
+document.addEventListener('click', (e) => {
+  if (!document.querySelector('.main-nav').contains(e.target)) {
+    burger.classList.remove('main-nav__burger--active');
+    mobile_menu.classList.remove('main-nav__nav-menu--mobile--active');
+    document.body.classList.remove('body-cover');
+    unlock(mobile_menu);
+  }
+});
 
 // - Закрытие по клику на пункт меню
 nav_list.onclick = function (event) {
@@ -29,6 +39,24 @@ nav_list.onclick = function (event) {
 
   burger.classList.toggle('main-nav__burger--active');
   mobile_menu.classList.toggle('main-nav__nav-menu--mobile--active');
-  page_cover.classList.remove('cover-default--active');
+  document.body.classList.remove('body-cover');
   unlock(mobile_menu);
 };
+
+// --- Табы в хединге
+let tabs_buttons = document.querySelectorAll('.heading__column-tabs-button');
+let tabs_items = document.querySelectorAll('.heading__image');
+
+tabs_buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    tabs_buttons.forEach((button_item) => { button_item.classList.remove('heading__column-tabs-button--active') })
+    button.classList.add('heading__column-tabs-button--active');
+
+    tabs_items.forEach((item) => {
+      if (button.dataset.tab === item.dataset.tab) {
+        tabs_items.forEach((value) => { value.classList.remove('heading__image--active') });
+        item.classList.add('heading__image--active');
+      }
+    })
+  })
+})
